795a17ea1cbdb634ce7b5ab0335287f58588c864
ZbigniewLemiesz <lemiesz.zbigniew@gmail.com>
2024-12-11 18:04:41 +0100

    Demo of spring-boot-starter-actuator

--- src/main/java/com/example/demo/rest/FirstRestController.java
+++ src/main/java/com/example/demo/rest/FirstRestController.java
@@ -11,8 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class FirstRestController {
 
     // expose "/" that return "Hello World"
+
     @GetMapping("/")
     public String sayHello() {
         return "Hello World!";
+    }
+
+    // expose a new endpoint for "workout"
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Go for a walk";
+    }
+
+    // expose a new endpoint for "fortune"
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day.";
     }
 }
